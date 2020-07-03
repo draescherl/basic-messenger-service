4ed043e2185443ffd6a0a84674e1d74fbe74d214
Lucas Draescher <lucas.draescher@eisti.fr>
2020-07-03 15:11:36 +0200

    Register db

--- model/user-register.php
+++ model/user-register.php
@@ -1,2 +1,91 @@
 <?php
 
+function usernameAlreadyExists(string $username) : bool
+{
+    $db  = dbConnect();
+    $req = $db->prepare('SELECT username FROM users WHERE username = :username');
+    $req->execute(array(
+        'username' => $username
+    ));
+
+    return ($req->rowCount() > 0);
+}
+
+function isValid(string $item, string $type = 'password')
+{
+    $valid = true;
+    $error = '';
+
+    if ($type === 'username') {
+        // Check if username already exists :
+        if (usernameAlreadyExists($item)) {
+            $valid = false;
+            $error = 'exists';
+        }
+    }
+
+    // Check if username/password has a space in it :
+    if ($valid && preg_match('/\s/', $item)) {
+        $valid = false;
+        $error = 'space';
+    }
+
+    // Check if username/password length is longer than 20 characters :
+    if ($valid && (strlen($item) > 20)) {
+        $valid = false;
+        $error = 'long';
+    }
+
+    // Check if username/password length is shorter than 3 characters :
+    if ($valid && (strlen($item) < 3)) {
+        $valid = false;
+        $error = 'short';
+    }
+
+    $res = ($valid) ? true:$error;
+    return $res;
+}
+
+function passwordsMatch(string $password, string $confirm) : bool
+{
+    return ($password === $confirm);
+}
+
+function dbConnect()
+{
+    try {
+        $db = new PDO('mysql:host=localhost;dbname=messagerie;charset=utf8', 'root', '', array(PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION));
+        return $db;
+    } catch(Exception $e) {
+        die('Error : ' . $e->getMessage());
+    }
+}
+
+
+
+// Get data :
+$username = $_POST['username'];
+$password = $_POST['password'];
+$confirm  = $_POST['confirm'];
+
+// Check :
+$username_valid  = isValid($username, 'username');
+$password_valid  = isValid($password);
+$passwords_match = passwordsMatch($password, $confirm);
+
+if (!$passwords_match) {
+    echo 'not-match';
+} elseif ($password_valid !== true) {
+    echo 'password-' . $password_valid;
+} elseif ($username_valid !== true) {
+    echo 'username-' . $username_valid;
+} else {
+    $db  = dbConnect();
+    $req = $db->prepare('INSERT INTO users(username, password, role) VALUES (:username, :password, :role)');
+    $req->execute(array(
+        'username' => $username,
+        'password' => password_hash(htmlspecialchars($password), PASSWORD_DEFAULT),
+        'role' => 'user'
+    ));
+    echo 'success';
+}
--- view/auth/register.php
+++ view/auth/register.php
@@ -11,6 +11,12 @@ $style = ob_get_clean();
 ob_start(); 
 ?>
 
+<div class="container">
+    <div class="row">
+        <div id="results" class="col-sm-12 col-md-6 offset-md-3 mt-2" role="alert"></div>
+    </div>
+</div>
+
 <div class="wrapper fadeInDown">
     <div id="formContent">
         <!-- Icon -->
@@ -29,7 +35,7 @@ ob_start();
                     Show password
                 </label>
             </div>
-            <input type="submit" class="fadeIn fourth" value="Log In">
+            <input type="submit" id="submit" class="fadeIn fourth" value="Create account">
         </form>
     </div>
 </div>
@@ -66,11 +72,43 @@ ob_start(); ?>
                 },
 
                 function(data) {
-                    if (data == 'Success') {
-                        window.location.href = '/messagerie/?action=home';
+                    if (data == 'success') {
+                        //window.location.href = '/messagerie/?action=home';
+                        alert(data);
                     } else {
-                        $("#results").html("<p>Identifiant ou mot de passe incorrect.</p>");
-                        $("#results").addClass('card-panel white-text text-darken-2 red');
+                        switch (data) {
+                            case 'not-match': content = 'Passwords do not match.';
+                            break;
+
+                            case 'password-space': content = 'Password cannot contain spaces.';
+                            break;
+
+                            case 'password-long': content = 'Password must be less than (or equal to) 20 characters long.';
+                            break;
+
+                            case 'password-short': content = 'Password must be more than (or equal to) 3 characters long.';
+                            break;
+
+                            case 'username-space': content = 'Username cannot contain spaces.';
+                            break;
+
+                            case 'username-long': content = 'Username must be less than (or equal to) 20 characters long.';
+                            break;
+
+                            case 'username-short': content = 'Username must be more than (or equal to) 3 characters long.';
+                            break;
+
+                            case 'username-exists': content = 'Username already exists.';
+                            break;
+                        
+                            default: content = 'Internal error !';
+                            break;
+                        }
+                        content += '<button type="button" class="close" data-dismiss="alert" aria-label="Close">';
+                        content += '<span aria-hidden="true">&times;</span>';
+                        content += '</button>';
+                        $("#results").html(content);
+                        $("#results").addClass('alert alert-danger alert-dismissible fade show');
                     }
                 },
                 'text'
