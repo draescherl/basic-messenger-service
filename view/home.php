79243327882c6e49c39c3e7ad5f673045e688811
Thomas Olivier <thomas.olivier@eisti.fr>
2020-07-10 20:18:30 +0200

    Replace home.php

--- view/home.php
+++ view/home.php
@@ -13,21 +13,35 @@ $title = 'Accueil';
             </div>
             <!-- content of the left element -->
             <div class="card-body">
-                <p>Home</p>
-                <img src="static/img/trueno.jpg" class="rounded-circle mr-3" style="max-width: 70px; border:1px solid black;">
+                <div class="p-2 rounded-lg bg-secondary text-white"> <!-- selected user using this class -->
+                    <img src="static/img/trueno.jpg" class="rounded-circle mr-3" style="max-width: 70px; border:1px solid black;">
+                    <span><b>Username</b></span>
+                </div>
+                <hr>
+                <div>
+                    <img src="static/img/trueno.jpg" class="rounded-circle mr-3" style="max-width: 70px; border:1px solid black;">
+                    <span><b>Username 2</b></span>
+                </div>
             </div>
         </div>
 
         <!-- right element -->
-        <div class="card" style="width: 18rem;">
+        <div class="card" style="width: 50%;">
             <!-- title of the left element -->
             <div class="card-header">
                 Username
             </div>
             <!-- content of the left element -->
             <div class="card-body">
-                <p>One little message.</p>
-                <img src="static/img/trueno.jpg" class="rounded-circle mr-3" style="max-width: 70px; border:1px solid black;">
+                <div class="d-flex justify-content-end mb-2">
+                    <div class="border rounded-pill p-2">Is this ok?</div>
+                </div>
+                <div class="d-flex justify-content-start mb-2">
+                    <div class="border rounded-pill p-2">By seeing this I can say YES!!!</div>
+                </div>
+                <div class="d-flex justify-content-end mb-2">
+                    <div class="border rounded-pill p-2">Big oof</div>
+                </div>
             </div>
         </div>
     </div>
