712e2fda78b0c89dab3805de6643c719d5206a6d
Thomas Olivier <oliviertho@eisti.eu>
2020-07-31 14:39:18 +0200

    Added colors

--- view/home.php
+++ view/home.php
@@ -6,7 +6,7 @@ $title = 'Accueil';
 <div class="container-fluid">
     <div class="row">
         <!-- left element -->
-        <div class="card" style="width: 18rem;">
+        <div class="card" style="width: 15%;">
             <!-- title of the left element -->
             <div class="card-header">
                 Home
@@ -15,32 +15,32 @@ $title = 'Accueil';
             <div class="card-body">
                 <div class="p-2 rounded-lg bg-secondary text-white"> <!-- selected user using this class -->
                     <img src="static/img/trueno.jpg" class="rounded-circle mr-3" style="max-width: 70px; border:1px solid black;">
-                    <span><b>Username</b></span>
+                    <span><b>Username</b></span> <!-- !!!!!!!!!!!! We need to put the var username here !!!!!!! -->
                 </div>
                 <hr>
                 <div>
                     <img src="static/img/trueno.jpg" class="rounded-circle mr-3" style="max-width: 70px; border:1px solid black;">
-                    <span><b>Username 2</b></span>
+                    <span><b>Username 2</b></span> <!-- !!!!!!!!!!!! We need to put the var username here !!!!!!! -->
                 </div>
             </div>
         </div>
 
         <!-- right element -->
-        <div class="card" style="width: 50%;">
+        <div class="card" style="width: 85%;">
             <!-- title of the left element -->
             <div class="card-header">
-                Username
+                Username <!-- !!!!!!!!!!!! We need to put the var username here !!!!!!! -->
             </div>
             <!-- content of the left element -->
             <div class="card-body">
                 <div class="d-flex justify-content-end mb-2">
-                    <div class="border rounded-pill p-2">Is this ok?</div>
+                    <div class="border rounded-pill sent-message p-2">Is this ok?</div>
                 </div>
                 <div class="d-flex justify-content-start mb-2">
-                    <div class="border rounded-pill p-2">By seeing this I can say YES!!!</div>
+                    <div class="border rounded-pill received-message p-2">By seeing this I can say YES!!!</div>
                 </div>
                 <div class="d-flex justify-content-end mb-2">
-                    <div class="border rounded-pill p-2">Big oof</div>
+                    <div class="border rounded-pill sent-message p-2">Big oof</div>
                 </div>
             </div>
         </div>
