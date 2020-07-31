548c642d70cf0010cdc8b8da6a8f5e079880f012
Lucas Draescher <draescherl@eisti.eu>
2020-07-31 15:48:29 +0200

    Created insertMessage function

---
+++ model/insert-message.php
@@ -0,0 +1,16 @@
+<?php
+
+include 'utils.php';
+
+function insertMessage(int $senderID, int $receiverID, string $contents, int $deleted=0)
+{
+    $db    = dbConnect();
+    $query = 'INSERT INTO messages(sender-id, receiver-id, contents, deleted) VALUES(:sender, :receiver, :contents, :deleted)';
+    $data  = array(
+        'sender'    =>  $senderID,
+        'receiver'  =>  $receiverID,
+        'contents'  =>  $contents,
+        'deleted'   =>  $deleted
+    );
+    return $req->execute($data);
+}
