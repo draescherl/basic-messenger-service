6b55d393ae0d28cfe2bf424d0eedc24942802006
Lucas Draescher <draescherl@eisti.eu>
2020-07-31 15:20:02 +0200

    Created getMessages and getUsers functions

--- model/get-messages.php
+++ model/get-messages.php
@@ -1,3 +1,24 @@
 <?php
 
 include 'utils.php';
+
+function getMessages(int $senderID, int $receiverID)
+{
+    // Get data from database :
+    $db    = dbConnect();
+    $query = 'SELECT * FROM messages WHERE (sender-id = :sender AND receiver-id = :receiver) OR (sender-id = :receiver AND receiver-id = :sender) ORDER BY timestamp DESC';
+    $req   = $db->prepare($query);
+    $req->execute(array(
+        'sender'   => $senderID,
+        'receiver' => $receiverID
+    ));
+    $rawMessages = $req->fetchAll();
+
+    // Save only content :
+    $messages = array();
+    foreach ($rawMessages as $message) {
+        array_push($messages, $message['contents']);
+    }
+
+    return $messages;
+}
