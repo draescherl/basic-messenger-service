454cd3cf0d396724487c5c360ea4356e7cd9902f
Lucas Draescher <draescherl@eisti.eu>
2020-08-01 17:17:52 +0200

    Removed trailing whitespaces

--- view/home.php
+++ view/home.php
@@ -1,20 +1,17 @@
 <?php
 $title = 'Accueil';
 
-ob_start(); 
+ob_start();
 ?>
 
 <div class="container-fluid">
     <div class="row">
-        <!-- left element -->
+        <!-- Left part of the page -->
         <div class="card" style="width: 15%; min-height: 100vh;">
-            <!-- title of the left element -->
-            <div class="card-header">
-                Home
-            </div>
-            <!-- content of the left element -->
+            <div class="card-header"> Chats </div>
             <div class="card-body">
                 <?php foreach ($users as $user) { ?>
+
                 <a href=<?= "'" . "/messagerie/?action=home&to=" . $user . "'" ?>>
                     <div> <!-- class="p-2 rounded-lg bg-secondary text-white"> -->
                         <img src="static/img/trueno.jpg" class="rounded-circle mr-3" style="max-width: 70px; border:1px solid black;">
@@ -22,34 +19,30 @@ ob_start();
                     </div>
                 </a>
                 <hr>
+
                 <?php } ?>
             </div>
         </div>
 
-        <!-- right element -->
+        <!-- Right part of the page -->
         <div class="card" style="width: 85%;">
-            <!-- title of the left element -->
-            <div class="card-header">
-                <?= $_SESSION['username'] ?>
-            </div>
-            
-            <!-- content of the left element -->
+            <div class="card-header"> <?= $_SESSION['username'] ?> </div>
             <div class="card-body">
                 <?php if (isset($messages)) {
                     foreach ($messages as $message) { ?>
                         <?php if ($message['senderID'] === $_SESSION['id']): ?>
-                        
+
                         <div class="d-flex justify-content-end mb-2">
                             <div class="border rounded-pill p-2" style="background-color: #0084ff; color: #fff;"> <?= $message['contents'] ?> </div>
                         </div>
-                        
+
                         <?php else: ?>
-                        
+
                         <div class="d-flex justify-content-start mb-2">
                             <div class="border rounded-pill p-2" style="background-color: #f1f0f0; color: rgba(0, 0, 0, 1);"> <?= $message['contents'] ?> </div>
                         </div>
-                        
-                        <?php endif; 
+
+                        <?php endif;
                     }
                 } ?>
             </div>
@@ -60,7 +53,7 @@ ob_start();
     </div>
 </div>
 
-<?php 
-$content = ob_get_clean(); 
-require('view/template.php'); 
+<?php
+$content = ob_get_clean();
+require('view/template.php');
 ?>
